21d2f60b81a16e854aba597d62e8295132fc1fef
Gabe-Real <you@example.com>
2025-06-01 15:03:03 +0100

    Make the items work with 1.21.5 because it has this stupid thing that no other version I've ever modded before has.

--- src/main/java/dev/gabereal/items/ModItems.java
+++ src/main/java/dev/gabereal/items/ModItems.java
@@ -6,15 +6,20 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
-public class ModItems {
-    public static final Item HEART_CONTAINER = registerItem("heart_container", new Item(new Item.Settings()));
-    public static final Item LARGE_HEART_CONTAINER = registerItem("large_heart_container", new Item(new Item.Settings()));
-    public static final Item DARK_HEART = registerItem("dark_heart", new Item(new Item.Settings()));
+import java.util.function.Function;
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Atria.MOD_ID, name), item);
+public class ModItems {
+    public static final Item HEART_CONTAINER = registerItem("heart_container", Item::new);
+    public static final Item LARGE_HEART_CONTAINER = registerItem("large_heart_container", Item::new);
+    public static final Item DARK_HEART = registerItem("dark_heart", Item::new);
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(Atria.MOD_ID, name),
+         function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Atria.MOD_ID, name)))));
     }
 
     public static void registerModItems() {
