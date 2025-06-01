b8312295c5f8760f09a49637c6dd04666725f466
Gabe-Real <you@example.com>
2025-06-01 15:06:44 +0100

    Another item

--- src/main/java/dev/gabereal/items/ModItems.java
+++ src/main/java/dev/gabereal/items/ModItems.java
@@ -16,6 +16,7 @@ public class ModItems {
     public static final Item HEART_CONTAINER = registerItem("heart_container", Item::new);
     public static final Item LARGE_HEART_CONTAINER = registerItem("large_heart_container", Item::new);
     public static final Item DARK_HEART = registerItem("dark_heart", Item::new);
+    public static final Item HEART = registerItem("heart", Item::new);
 
     private static Item registerItem(String name, Function<Item.Settings, Item> function) {
         return Registry.register(Registries.ITEM, Identifier.of(Atria.MOD_ID, name),
@@ -29,6 +30,7 @@ public class ModItems {
             entries.add(HEART_CONTAINER);
             entries.add(LARGE_HEART_CONTAINER);
             entries.add(DARK_HEART);
+            entries.add(HEART);
         });
     }
 }
