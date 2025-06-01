a01187349020669bc4109bb9dce1f5179016bc1b
Gabe-Real <you@example.com>
2025-06-01 12:42:50 +0100

    Inital stuff

---
+++ src/client/java/dev/gabereal/AtriaClient.java
@@ -0,0 +1,10 @@
+package dev.gabereal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtriaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
